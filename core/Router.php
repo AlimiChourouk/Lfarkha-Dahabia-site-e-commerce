de82eaccc819e4eda42cb10b69ce866abff03657
AlimiChourouk <Alimi.Chourouk.solicode@gmail.com>
2025-05-29 21:22:39 +0100

    Ajout des routes 'contact' et 'contact/envoyer' pour la gestion des messages

--- core/Router.php
+++ core/Router.php
@@ -3,6 +3,7 @@ require_once '../config/db.php';
 $controllers = [
         'AccueilController' => '../app/controllers/AccueilController.php',
         'InscriptionController' => '../app/controllers/InscriptionController.php',
+        'MessageController' => '../app/controllers/MessageController.php',
     ];
     foreach ($controllers as $file) {
         require_once $file;
@@ -24,7 +25,14 @@ class Router {
                 $this->checkPost();
                 (new InscriptionController())->register();
                 exit;    
+            case 'contact':
+                (new MessageController($GLOBALS['pdo']))->index();
+                exit;
 
+            case 'contact/envoyer':
+            $this->checkPost();
+            (new MessageController($GLOBALS['pdo']))->envoyer();
+            exit;
             default:
                 echo "Page non trouvée.";
                 break;
